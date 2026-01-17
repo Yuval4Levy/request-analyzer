c0ddff587499c6b26f18637b8f94136ebf2e9585
yuvallevy <yuval4levy@gmail.com>
2026-01-17 13:23:37 +0200

    [fix] maven lombock issue

--- src/main/java/org/yuval/requestsAnalyzer/objects/RequestData.java
+++ src/main/java/org/yuval/requestsAnalyzer/objects/RequestData.java
@@ -1,10 +1,5 @@
 package org.yuval.requestsAnalyzer.objects;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class RequestData {
     // region Members
     private String country;
@@ -15,6 +10,30 @@ public class RequestData {
     public RequestData(String country, String operatingSystem, String browser) {
         this.country = country;
         this.operatingSystem = operatingSystem;
+        this.browser = browser;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
         this.browser = browser;
     }
 }
